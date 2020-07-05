8d47898dccac2a1332ce7d1d2412c73e5d21e489
BaptisteBorne <60301250+BaptisteBorne@users.noreply.github.com>
2020-07-05 15:12:26 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,66 +1,93 @@
 //
 //  main.cpp
-//  Binomial Tree
+//  MC_EuropeanOption
 //
-//  Created by Baptiste Borne on 24/12/2018.
-//  Copyright © 2018 Baptiste Borne. All rights reserved.
+//  Created by Baptiste Borne on 24/02/2019.
+//  Copyright © 2019 Baptiste Borne. All rights reserved.
 //
 
-#include <iostream>
+#include <algorithm>    // Needed for the "max" function
 #include <cmath>
-#include <vector>
-#include <ctime>
+#include <iostream>
 
-#include "BinomialTree.h"
-#include "AmericanOption.h"
+// plutôt que Box-Muller ?
+//#include <random>
+//std::normal_distribution<>
 
-#include "EuropeanOption.h"
+// A simple implementation of the Box-Muller algorithm, used to generate
+// gaussian random numbers - necessary for the Monte Carlo method below
+// Note that C++11 actually provides std::normal_distribution<> in
+// the <random> library, which can be used instead of this function
+double gaussian_box_muller() {
+    double x = 0.0;
+    double y = 0.0;
+    double euclid_sq = 0.0;
+    
+    // Continue generating two uniform random variables
+    // until the square of their "euclidean distance"
+    // is less than unity
+    do {
+        x = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
+        y = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
+        euclid_sq = x*x + y*y;
+    } while (euclid_sq >= 1.0);
+    
+    return x*sqrt(-2*log(euclid_sq)/euclid_sq);
+}
 
-using namespace std;
+// Pricing a European vanilla call option with a Monte Carlo method
+double monte_carlo_call_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
+    double S_adjust = S * exp(T*(r-0.5*v*v));
+    double S_cur = 0.0;
+    double payoff_sum = 0.0;
+    
+    for (int i=0; i<num_sims; i++) {
+        double gauss_bm = gaussian_box_muller();
+        S_cur = S_adjust * exp(sqrt(v*v*T)*gauss_bm);
+        payoff_sum += std::max(S_cur - K, 0.0);
+    }
+    
+    return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
+}
 
-int main(){
-/*
-    cout<< "Valeur sous-jacent à t=0 : " ;
-    double So_user;
-    cin>>So_user;
-    cout<< "Strike : ";
-    double K_user;
-    cin>>K_user;
-    cout<< "Risk-free rate : ";
-    double r_user;
-    cin>> r_user;
-    cout<< "Maturité option : ";
-    double T_user;
-    cin>>T_user;
-    cout<< "Volatilité : ";
-    double sigma_user;
-    cin>> sigma_user;
-    cout<< "Nombre de sous-périodes : ";
-    double N_user;
-    cin>> N_user;
-    */
-    // pr l instant on prend un call americain
-    clock_t t;
-    t=clock();
-   
-    //AmericanOption optionA(So_user,K_user, T_user, r_user,
-     //                      sigma_user,"call",N_user);
+// Pricing a European vanilla put option with a Monte Carlo method
+double monte_carlo_put_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
+    double S_adjust = S * exp(T*(r-0.5*v*v));
+    double S_cur = 0.0;
+    double payoff_sum = 0.0;
     
-    //optionA.display();
-   
+    for (int i=0; i<num_sims; i++) {
+        double gauss_bm = gaussian_box_muller();
+        S_cur = S_adjust * exp(sqrt(v*v*T)*gauss_bm);
+        payoff_sum += std::max(K - S_cur, 0.0);
+    }
     
-    AmericanOption optionA(50,52,2,0.05,0.7,"put",10000);
-    //EuropeanOption optionB(50,52,2,0.05,0.7,"put",10000);
-    optionA.display();
-    //optionB.display();
+    return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
+}
+
+int main(int argc, char **argv) {
+    // First we create the parameter list
+    int num_sims = 10000000;   // Number of simulated asset paths
+    double S = 40.0;  // Option price
+    double K = 50.0;  // Strike price
+    double r = 0.05;   // Risk-free rate (5%)
+    double v = 0.7;    // Volatility of the underlying (20%)
+    double T = 2.0;    // One year until expiry
     
-    //EuropeanOption optiontest(25,30,1,0.05,0.2,"call",100);
-    //optiontest.display();
+    // Then we calculate the call/put values via Monte Carlo
+    double call = monte_carlo_call_price(num_sims, S, K, r, v, T);
+    double put = monte_carlo_put_price(num_sims, S, K, r, v, T);
     
-    t=clock()-t;
-    cout<<"..................................."<<endl;
-    cout<<"Calculation time: " << ((float)t)/CLOCKS_PER_SEC<< " secondes" <<endl;
-    cout<<"..................................."<<endl;
+    // Finally we output the parameters and prices
+    std::cout << "Number of Paths: " << num_sims << std::endl;
+    std::cout << "Underlying:      " << S << std::endl;
+    std::cout << "Strike:          " << K << std::endl;
+    std::cout << "Risk-Free Rate:  " << r << std::endl;
+    std::cout << "Volatility:      " << v << std::endl;
+    std::cout << "Maturity:        " << T << std::endl;
+    
+    std::cout << "Call Price:      " << call << std::endl;
+    std::cout << "Put Price:       " << put << std::endl;
     
     return 0;
 }
